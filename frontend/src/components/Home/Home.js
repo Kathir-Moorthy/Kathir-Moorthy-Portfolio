45820547e9e60a2821385f7b8e61521de52378e1
Kathir-Moorthy <kathirmoorthybsb@gmail.com>
2025-02-16 15:08:06 +0530

    Updated my resume

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -141,7 +141,7 @@ function Home() {
                 }}
             >
                 <a
-                    href="https://drive.google.com/file/d/1r89jp5mZsn5QB1tSi7CB1P6zXLtwi3_F/view?usp=drive_link"
+                    href="https://drive.google.com/file/d/1NTNQwWfPNC8NqzxhN6pr61AueWYGhBHF/view?usp=drive_link"
                     target="_blank"
                     download
                     className={`relative inline-block px-6 py-3 font-semibold rounded-full no-underline transition-all duration-300 ${
